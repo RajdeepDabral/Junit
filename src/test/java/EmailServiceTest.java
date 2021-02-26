5dbce6690a6e6adfd39899b40c3c604a83af1a70
rajdeep-dabral <mail.rajdeepdabral@gmail.com>
2021-02-26 20:38:48 +0530

    Question Updated!

--- src/test/java/EmailServiceTest.java
+++ src/test/java/EmailServiceTest.java
@@ -1,21 +1,20 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class EmailServiceTest {
+class EmailServiceTest {
     public static EmailService emailService=new EmailService();
-    
+
     @Test
-    public void createStaticInstance(){
-        Assertions.assertEquals(EmailService.getInstance(),EmailService.getInstance()
+    void createStaticInstance(){
+        assertEquals(EmailService.getInstance(),EmailService.getInstance()
                 ,"static instance are pointing to same Object");
     }
     @Test
-    public void testSendEmailWithOrder(){
-        Assertions.assertThrows(RuntimeException.class , () -> emailService.sendEmail(new Order()));
+    void testSendEmailWithOrder(){
+        assertThrows(RuntimeException.class , () -> emailService.sendEmail(new Order()));
     }
     @Test
-    public void testSendEmailWithOrderAndString(){
-        Assertions.assertTrue(emailService.sendEmail(new Order() , new String()));
+    void testSendEmailWithOrderAndString(){
+        assertTrue(emailService.sendEmail(new Order() , new String()));
     }
 }
--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,16 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class OrderTest {
-    public static Order orderBook=new Order().setPrice(50).setItemName("Book").setQuantity(25).setPriceWithTex(1250.0);;
-    public static Order orderPen =new Order(10 , "Pen" ,50.55).setPriceWithTex(505.5);
-
+class OrderTest {
 
     @Test
     @DisplayName("Constructor with Parameter values")
-    public void testOrderConstructorWithParameterValue(){
+    void testOrderConstructorWithParameterValue(){
+        Order orderPen =new Order(10 , "Pen" ,50.55).setPriceWithTex(505.5);
         int quantity= 10;
         String itemName="Pen";
         double price = 50.55;
@@ -24,7 +22,8 @@ public class OrderTest {
     }
     @Test
     @DisplayName("Constructor without Parameter values")
-    public void testOrderConstructorWithoutParameterValue(){
+    void testOrderConstructorWithoutParameterValue(){
+        Order orderBook=new Order().setPrice(50).setItemName("Book").setQuantity(25).setPriceWithTex(1250.0);
         int quantity= 25;
         String itemName="Book";
         double price = 50;
@@ -36,4 +35,74 @@ public class OrderTest {
                 () -> assertEquals(priceTax, orderBook.getPriceWithTex())
         );
     }
+    @Nested
+    class OrderWithoutData{
+        private Order orderWithoutData;
+        @Test
+        void testOrderWithoutItemName(){
+            orderWithoutData = new Order().setPrice(10)
+                                    .setQuantity(10)
+                                    .setPriceWithTex(100);
+            assertNull(orderWithoutData.getItemName(),"ItemName is Null");
+        }
+        @Test
+        void testOrderWithoutPrice(){
+            orderWithoutData = new Order().setQuantity(10)
+                                    .setPriceWithTex(100)
+                                    .setItemName("Cap");
+            assertEquals(0.0d,orderWithoutData.getPrice(),"Price is Not Provided");
+        }
+        @Test
+        void testOrderWithoutQuantity(){
+            orderWithoutData = new Order()
+                                .setPrice(10)
+                                .setPriceWithTex(100)
+                                .setItemName("Cap");
+            assertEquals(0,orderWithoutData.getQuantity(),"Quantity not Provided");
+        }
+        @Test
+        void testOrderWithoutPriceTax(){
+            orderWithoutData = new Order()
+                            .setPrice(10)
+                            .setQuantity(10)
+                            .setItemName("Cap");
+            assertEquals(0.0d,orderWithoutData.getItemName(),"Price tax not provided");
+        }
+
+    }
+    @Nested
+    class OrderWithNegativeOrZeroValue{
+        private Order orderWithNegativeOrZero = null;
+        @Test
+        void testOrderWithNegativePrice(){
+            orderWithNegativeOrZero =  new Order().setPrice(-100).setQuantity(0)
+                    .setItemName("Box").setPriceWithTex(0);
+            assertEquals(-100,orderWithNegativeOrZero.getPrice(),"Price cannot be negative");
+        }
+        @Test
+        void testOrderWithNegativePriceTax(){
+            orderWithNegativeOrZero =  new Order().setPrice(100).setQuantity(100)
+                    .setItemName("Box").setPriceWithTex(-100);
+            assertEquals(-100,orderWithNegativeOrZero.getPriceWithTex(),"Price tax cannot be negative");
+        }
+        @Test
+        void testOrderWithZeroQuantity(){
+            orderWithNegativeOrZero =  new Order().setPrice(100).setQuantity(0)
+                    .setItemName("Box").setPriceWithTex(10);
+            assertEquals(0,orderWithNegativeOrZero.getQuantity(),"Quantity cannot be zero");
+        }
+        @Test
+        void testOrderWithZeroPriceTax(){
+            orderWithNegativeOrZero =  new Order().setPrice(100).setQuantity(100)
+                    .setItemName("Box").setPriceWithTex(0);
+            assertEquals(0,orderWithNegativeOrZero.getPriceWithTex(),"PriceTax cannot be zero");
+        }
+        @Test
+        void testOrderWithZeroPrice(){
+            orderWithNegativeOrZero =  new Order().setPrice(0).setQuantity(10)
+                    .setItemName("Box").setPriceWithTex(10);
+            assertEquals(0,orderWithNegativeOrZero.getPrice(),"Price cannot be zero");
+        }
+    }
+
 }
